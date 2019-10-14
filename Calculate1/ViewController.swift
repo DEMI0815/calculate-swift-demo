4e06e6120fa6f086f1abd95a95de0856f02feaf6
Yidi <yidi.zhao@thoughtworks.com>
2019-10-14 18:33:49 +0800

    Initial Commit

---
+++ Calculate1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculate1
+//
+//  Created by Yidi Zhao on 2019/10/14.
+//  Copyright © 2019 Yidi Zhao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
